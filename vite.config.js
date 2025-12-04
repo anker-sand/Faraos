5252a4f644203f82662a5e89a1f2f4d06113da81
ankerderbanker <anker2000@hotmail.com>
2025-12-05 00:34:26 +0100

    Revert "chore: add GitHub Pages deploy workflow"
    This reverts commit f691600c76abdc7ce0bfa67f70e83882d7f08d78.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Faraos/", // Set this to your repository name
   plugins: [react(), tailwindcss()],
 });
