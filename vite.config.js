a7760cf3ebc4fbac9c159ff3975c2d27168c9f13
ankerderbanker <anker2000@hotmail.com>
2025-12-16 21:00:59 +0100

    prøv vercel fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Faraos/',
+  base: "/Faraos/",
   plugins: [react(), tailwindcss()],
   safelist: [
     "bg-neutral-950",
